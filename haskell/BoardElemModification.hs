f5819a1db151c348ef27b3cfabad449eb416ea7b
João Victor Teodulo <joao.wanderley@ccc.ufcg.edu.br>
2020-11-08 04:48:41 -0300

    add insert and remove

---
+++ haskell/BoardElemModification.hs
@@ -0,0 +1,16 @@
+module BoardElemModification(
+    removeNumber,
+    insertNewNumber
+) where
+
+modifyNumber :: [[Int]] -> Int -> (Int, Int) -> [[Int]]
+modifyNumber matrix x (row,column) =
+  take row matrix ++
+  [take column (matrix !! row) ++ [x] ++ drop (column + 1) (matrix !! row)] ++
+  drop (row + 1) matrix
+
+removeNumber :: [[Int]] -> (Int, Int) -> [[Int]]
+removeNumber matrix (row,column) = modifyNumber matrix 0 (row,column)
+
+insertNewNumber :: [[Int]] -> Int -> (Int, Int) -> [[Int]]
+insertNewNumber matrix x (row,column) = modifyNumber matrix x (row,column)
