a27050bc09ecd7c23121aa433d92cad96c4a3774
caiocnborges <45263528+caiocnborges@users.noreply.github.com>
2020-11-30 19:55:50 -0300

    Create check.pl
    add método de checagem comparativa

---
+++ check.pl
@@ -0,0 +1,41 @@
+% Checking - Checagem comparativa do tabuleiro em jogo com o tabuleiro-resposta
+
+checkTable(ListBoard, CompleteBoard):-
+  checkTable(ListBoard, 0, CompleteBoard).
+
+checkTable([H], N, CompleteBoard):-
+  checkTable(H, N, 0, CompleteBoard),
+  !.
+
+checkTable([H|T], N, CompleteBoard):-
+  checkTable(H, N, 0, CompleteBoard),
+  NTemp is N + 1,
+  checkTable(T, NTemp, CompleteBoard).
+
+checkTable([_], N, M, CompleteBoard):-
+  checkCell(N, M, CompleteBoard),
+  !.
+
+checkTable([_|T], N, M, CompleteBoard):-
+  checkCell(N, M, CompleteBoard),
+  MTemp is M + 1,
+  checkTable(T, N, MTemp, CompleteBoard).
+
+checkCell(N, M, Board):-
+  findNeighborVertival([N, M], List0),
+  findNeighborHorizontal([N, M], List1),
+  findNeighborSquare([N, M], List2),
+  getNeighborsValue(CompleteBoard, List0, Values0),
+  getNeighborsValue(CompleteBoard, List1, Values1),
+  getNeighborsValue(CompleteBoard, List2, Values2),
+  permutation(Values0, Values1),
+  permutation(Values1, Values2),
+  permutation(Values2, [1, 2, 3, 4, 5, 6, 7, 8, 9]).
+
+% Opção de checking simplificada // Testing
+
+% compare_matrix(Board, CompleteBoard) :-
+%   matrix(Board, I, J, Value1),
+%   matrix(CompleteBoard, I, J, Value2),
+%   Value1 == Value2,
+%   fail.
