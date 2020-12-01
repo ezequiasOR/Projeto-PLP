714a05b450d9cc20a8f652009c3b091edd43199f
caiocnborges <45263528+caiocnborges@users.noreply.github.com>
2020-11-30 23:29:58 -0300

    update check3
    update check3

--- prolog/check.pl
+++ prolog/check.pl
@@ -34,12 +34,12 @@ checkCell(N, M, Board):-
 
 % Opção de checking simplificada // Testing
 
-    compare_matrix(Board, CompleteBoard) :-
-    matrix(Board, I, J, Value1),
-    matrix(CompleteBoard, I, J, Value2),
-    Value1 == Value2.
+%    compare_matrix(Board, CompleteBoard) :-
+%    matrix(Board, I, J, Value1),
+%    matrix(CompleteBoard, I, J, Value2),
+%    Value1 == Value2.
 
-compararTabuleiros(Board, CompleteBoard) :-
-  (compare_matrix(Board, CompleteBoard) -> 
-  writeln("PARABÉNS, VOCÊ GANHOU!");
-  writeln("DISCREPÂNCIA ENCONTRADA")).
+%  compararTabuleiros(Board, CompleteBoard) :-
+%  (compare_matrix(Board, CompleteBoard) -> 
+%  writeln("PARABÉNS, VOCÊ GANHOU!");
+%  writeln("DISCREPÂNCIA ENCONTRADA")).
